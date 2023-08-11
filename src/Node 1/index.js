5acd60e4a6bf184c449b6dafffab1ca55e7d6172
NguyenCuong32 <141486946+NguyenCuong32@users.noreply.github.com>
2023-08-11 17:56:15 +0700

    Add files via upload
    Add source code

---
+++ src/Node 1/index.js
@@ -0,0 +1,17 @@
+const hello ="Hello world";
+console.log(hello);
+
+// Funtions
+var wakeUp = function(){
+    console.log("I'am waking up");
+}
+var checkPhone = function(){
+    console.log("I am checking phone");
+}
+var haveBreakfast = function(){
+    console.log("I' am eating breakfast ...");
+}
+
+wakeUp();
+checkPhone();
+haveBreakfast();
