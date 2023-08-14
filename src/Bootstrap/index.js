791311e65b5fefcfce79a98f7f3274d87dbdc34e
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-08-14 20:06:04 +0700

    Bootstrap + EJS demo B3

--- src/Bootstrap/index.js
+++ src/Bootstrap/index.js
@@ -1,14 +1,37 @@
-const express = require("express")
-const path = require('path')
 
+// Importing express module
+const express = require("express");
+const path = require('path')
+const port = 9002;
 const app = express();
 
+
+app.use(express.json());
+ 
+// For serving static HTML files
+app.use(express.static("public"));
+app.use('/',express.static(path.join(__dirname,"node_modules/bootstrap/dist")))
+app.use("/",express.static("node_modules/bootstrap/dist/"))
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/user.ejs'))
+    res.set({
+        "Allow-access-Allow-Origin": "*",
+    });
+ 
+    // res.send("Hello World");
+    return res.render('user.ejs');
+});
+
+// Handling the get request
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+// Starting the server on the 80 port
+app.listen(port, () => {
+	console.log(`The application started successfully on port ${port}`);
 });
 
 
-
-app.listen(9002, () => {
-    console.log(`Example app listening at 9001`)
-});
