5717fa7227acbd466c7dfc7d9d4179a8e0583e90
q5nguyenn <q5nguyenn@gmail.com>
2023-08-16 09:52:08 +0700

    Day03 update pagination

--- src/Day03/server.js
+++ src/Day03/server.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', './');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+const per_page = 5;
 
 const port = 3000;
 const fs = require('fs');
@@ -17,31 +18,31 @@ const fs = require('fs');
 app.get('/', async (req, res) => {
   console.log(__dirname);
   let users = await user.listUsers();
-  res.render('index', { users });
+  let currentPage = 1;
+  let pages = Math.ceil(users.length / per_page);
+  let begin = 0;
+  users = users.splice(begin, per_page);
+  res.render('index', { users, pages, currentPage });
+});
+
+app.get('/page/:page', async (req, res) => {
+  let users = await user.listUsers();
+  let currentPage = req.params.page;
+  let begin = (currentPage - 1) * per_page;
+  let pages = Math.ceil(users.length / per_page);
+  users = users.splice(begin, per_page);
+  res.render('index', { users, pages, currentPage });
 });
 
 //Add new user
-app.get('/user/add', async (req, res) => {
-  res.render('add');
-});
-
 app.post('/user/store', async (req, res) => {
   let users = await user.listUsers();
   let newUser = req.body;
   newUser.id = utility.generateNewId(users);
   await user.addUser(newUser);
-  res.redirect('/');
-});
-
-app.get('/user/edit/:id', async (req, res) => {
-  let id = parseInt(req.params.id);
-  let users = await user.listUsers();
-  let currentUser = users.find((user) => user.id == id);
-  if (currentUser) {
-    res.render('edit', { user: currentUser });
-  } else {
-    res.send('User does not exist!');
-  }
+  users = await user.listUsers();
+  let pages = Math.ceil(users.length / per_page);
+  res.redirect(`/page/${pages}`);
 });
 
 app.post('/user/update/:id', async (req, res) => {
@@ -55,7 +56,7 @@ app.post('/user/update/:id', async (req, res) => {
   }
   newUser.id = id;
   await user.updateUser(newUser);
-  res.redirect('/');
+  res.redirect('back');
 });
 
 app.get('/user/delete/:id', async (req, res) => {
@@ -67,7 +68,7 @@ app.get('/user/delete/:id', async (req, res) => {
     return;
   }
   await user.deleteUser(id);
-  res.redirect('/');
+  res.redirect('back');
 });
 
 app.listen(port, () => {
