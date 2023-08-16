ed26175b268d0a01e49ad5a5b644a8d63d52fcdd
Nguyen Giang <giang8692@gmail.com>
2023-08-16 19:23:23 +0700

    add function to call api from user.json file

--- src/Baitap1/index.js
+++ src/Baitap1/index.js
@@ -1,27 +1,25 @@
-const express = require("express");
-
-const port = 90;
+const express = require('express');
+const fs = require('fs');
 const app = express();
+const port = 3000; // You can change this to any port you prefer
 
+app.get('/api/getuser', (req, res) => {
+  fs.readFile('data/user.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).send('Internal Server Error');
+    }
 
-app.use(express.json());
- 
-// For serving static HTML files
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.set({
-        "Allow-access-Allow-Origin": "*",
-    });
- 
-    // res.send("Hello World");
-    return res.redirect("home.html");
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 });
 
-// Handling the get request
-
-// Starting the server on the 80 port
 app.listen(port, () => {
-	console.log(`The application started successfully on port ${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
