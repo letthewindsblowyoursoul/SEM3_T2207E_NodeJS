a8481cd6813b7d2964984b7ba6f20fb535c93f9b
Nguyen Giang <giang8692@gmail.com>
2023-08-19 09:05:42 +0700

    add user, remove user but not by ajax

--- src/Pratice/app.js
+++ src/Pratice/app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv');
 const uniqueValidator = require('mongoose-unique-validator');
+const path = require('path');
 
 dotenv.config({ path: "./config.env"});
+
 var app = express();
 
 mongoose.connect(process.env.DATABASE_LOCAL)
@@ -15,6 +17,7 @@ mongoose.connect(process.env.DATABASE_LOCAL)
                     console.log("Finally...");
 })
 
+
 var userSchema = mongoose.Schema({
                     userid: { type: Number, unique: true },
                     username: String,
@@ -22,44 +25,65 @@ var userSchema = mongoose.Schema({
                     address: String,
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator, { message: '{VALUE} already exists.' });
                 
 const User = mongoose.model("User", userSchema);
 app.use(express.json());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false  }));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
-app.get("/", (req, res) => {
-    res.set({
-        "Allow-access-Allow-Origin": "*",
-    });
- 
-    // res.send("Hello World");
-    return res.redirect("index.html");
-});
+app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).json({ error: 'Error fetching users' });
+    }
+  });
 
+app.post('/add-user', async (req, res) => {
+  const { username, fullname, address } = req.body;
 
-app.post("/add-user", async (req , res) => {
-    const { username, fullname, address } = req.body;
+  try {
 
-    
+    const randomUserid = Math.floor(Math.random() * 100000);
     const newUser = new User({
-        username,
-        fullname,
-        address
-      });
+      userid: randomUserid,
+      username,
+      fullname,
+      address
+    });
 
-    await newUser
-    .save()
-    .then((doc) => {
-        console.log(doc);
-        res.status(201).json({message: "User create successfully", data : doc});
-    })
-    .catch((err) => {
-        console.error("Error creating user:", err);
-        res.status(500).json({error : "Unable to create user"});
-    })
+    await newUser.save();
+    console.log('User inserted:', newUser);
+    res.json({ message: 'User inserted successfully' });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      console.error('Validation Error:', err.errors);
+      res.status(400).json({ error: 'Validation error', details: err.errors });
+    } else {
+      console.error('Error inserting user:', err);
+      res.status(500).json({ error: 'Error inserting user' });
+    }
+  }
 });
+
+app.delete('/remove-user/:id', async (req, res) => {
+    const userId = req.params.id;
+  
+    try {
+      await User.findByIdAndRemove(userId);
+      res.json({ message: 'User removed successfully' });
+    } catch (err) {
+      console.error('Error removing user:', err);
+      res.status(500).json({ error: 'Error removing user' });
+    }
+});
+
 
 app.listen(9000,"127.0.0.1",()=>{
     console.log("Listening to request on port 9000");
--- src/Pratice/public/js/script.js
+++ src/Pratice/public/js/script.js
@@ -1,12 +1,51 @@
-$(document).ready(function () {
-    $("#submit").click(function () {
-       $.post("/request",
-          {
-             name: "viSion",
-             designation: "Professional gamer"
-          },
-          function (data, status) {
-             console.log(data);
-          });
-    });
- });
+function fetchUsers() {
+   fetch('/users')
+     .then(response => response.json())
+     .then(data => {
+       const userList = document.getElementById('userList');
+       userList.innerHTML = '';
+
+       data.forEach(user => {
+         const li = document.createElement('li');
+         li.textContent = `${user.username} (${user.fullname}, ${user.address})`;
+
+         const removeButton = document.createElement('button');
+         removeButton.textContent = 'Remove';
+         removeButton.addEventListener('click', () => removeUser(user._id));
+
+         li.appendChild(removeButton);
+         userList.appendChild(li);
+       });
+     })
+     .catch(error => console.error('Error fetching users:', error));
+ }
+
+ // Function to remove a user
+ function removeUser(userId) {
+   fetch(`/remove-user/${userId}`, {
+     method: 'DELETE'
+   })
+     .then(response => response.json())
+     .then(data => {
+       console.log('User removed:', data);
+       fetchUsers();
+     })
+     .catch(error => console.error('Error removing user:', error));
+ }
+ // Initial fetch
+ fetchUsers();
+
+ // Submit form using AJAX
+
+ $(document).ready(function () {
+   $("#submit").click(function () {
+      $.post("/request",
+         {
+            name: "viSion",
+            designation: "Professional gamer"
+         },
+         function (data, status) {
+            console.log(data);
+         });
+   });
+});
