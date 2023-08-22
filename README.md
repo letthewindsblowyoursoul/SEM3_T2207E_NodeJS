0ff961cf66bd0bc86abd020093d834c3b6797c5a
letuananh1118 <tuananhlee1811@gmail.com>
2023-08-22 09:46:40 +0700

    delete all

--- README.md
+++
@@ -1,2 +0,0 @@
-# SEM3_T2207E_NodeJS
-SEM 3 T2207E Node JS
--- src/Node 1/Node 1-1/index.js
+++
@@ -1,9 +0,0 @@
-const post = require('./post.js');
-const user = require('./user.js');
-
-post.createPost();  
-post.commentOnPost();
-
-
-user.createUser();
-user.deleteUser();
