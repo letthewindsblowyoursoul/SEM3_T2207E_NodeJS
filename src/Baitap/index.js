49e60b1edf0e306dd5536ba9d5a0ff189075361c
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-08-23 16:28:18 +0700

    nopbt

--- src/Baitap/index.js
+++ src/Baitap/index.js
@@ -11,8 +11,8 @@ dotenv.config({ path: "./config/config.env" });
 
 var app = express();
 app.use(express.static("public"));
-app.use('/',express.static(path.join(__dirname,"node_modules/bootstrap/dist")))
-app.use("/",express.static("node_modules/bootstrap/dist/"))
+app.use('/', express.static(path.join(__dirname, "node_modules/bootstrap/dist")))
+app.use("/", express.static("node_modules/bootstrap/dist/"))
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -38,7 +38,8 @@ var userSchema = mongoose.Schema ({
     
 
 });
-
+const User = mongoose.model("User", userSchema);
+app.use(express.json());
 
 //--------------------------------------------------------------------------//
 
@@ -46,31 +47,28 @@ app.get("/", async (req, res) => {
     res.set({
         "Allow-access-Allow-Origin": "*",
     });
- 
-    // res.send("Hello World");
     try {
         const users = await User.find();
         res.render('Home.ejs', { users });
-      } catch (error) {
+    } catch (error) {
         res.status(500).send('Internal Server Error');
-      }
+    }
 });
 
 //--------------------------------------------------------------------------//
 
-const User = mongoose.model("User", userSchema);
-app.use(express.json());
-app.post("/post", (req, res) =>{
+
+app.post("/post", (req, res) => {
     const newUser = new User(req.body);
     newUser.save()
-    .then((doc) => {
-        console.log(doc);
-        res.status(201).json({ message: "User created successful" , data: doc})
-    })
-    .catch((err) => {
-        console.error("Erro creating user", err);
-        res.status(500).json({ error: "Unale to create user" })
-    })
+        .then((doc) => {
+            console.log(doc);
+            res.status(201).json({ message: "User created successful", data: doc })
+        })
+        .catch((err) => {
+            console.error("Erro creating user", err);
+            res.status(500).json({ error: "Unale to create user" })
+        })
 })
 
 //--------------------------------------------------------------------------//
@@ -80,12 +78,12 @@ app.delete('/delete/:id', async (req, res) => {
         const userId = req.params.userId;
         await User.findOneAndDelete({ UserId: userId });
         res.status(200).send('User deleted successfully');
-      } catch (error) {
+    } catch (error) {
         res.status(500).send('Internal Server Error');
-      }
+    }
 
-  });
+});
 
-app.listen(8080,() => {
+app.listen(8080, () => {
     console.log("Port 8080")
 })
